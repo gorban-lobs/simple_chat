b6db3c173cf99a369559aa4bed2c1b48d7447f02
Gorban Lobs <lobs.gorban@yandex.ru>
2019-09-05 01:03:59 +0300

    Add model Message

--- chat_project/chat/models.py
+++ chat_project/chat/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name='sender')
+    getter = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name='getter')
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
