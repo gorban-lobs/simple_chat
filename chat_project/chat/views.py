aaaacff129df127d08032ac69c3bb47f62f85846
Gorban Lobs <lobs.gorban@yandex.ru>
2019-09-05 01:43:55 +0300

    Add chat_list simple page

--- chat_project/chat/views.py
+++ chat_project/chat/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .models import Message
+
+
+def chat_list(request):
+	chat_list = User.objects.filter()
+	return render(request, 'chat/chat_list.html', {'chats': chat_list})
