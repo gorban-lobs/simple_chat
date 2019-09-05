d07e909eb128b1a7316c73a60ee4b44d6d4fe04e
Gorban Lobs <lobs.gorban@yandex.ru>
2019-09-05 15:34:37 +0300

    Add setting is_red to True when entering chat

--- chat_project/chat/views.py
+++ chat_project/chat/views.py
@@ -7,29 +7,35 @@ from .models import Message
 
 @login_required
 def chat_list(request):
-    cur_user_id = str(request.user.id)
+    cur_user_id = request.user.id
     query = '''
             SELECT u.id, username, count(m.sender_id) as cnt
             FROM auth_user u LEFT JOIN chat_message m ON
             u.id = m.sender_id AND  m.is_red = False AND m.getter_id = %s
             GROUP BY u.id HAVING u.id <> %s
             '''
-    chat_list = User.objects.raw(query, (cur_user_id, cur_user_id))
+    chat_list = User.objects.raw(query, (str(cur_user_id), str(cur_user_id)))
     return render(request, 'chat/chat_list.html', {'chats': chat_list})
 
 
 @login_required
 def chat_page(request, pk):
-    cur_user_id = str(request.user.id)
+    cur_user_id = request.user.id
     query = query = '''
             SELECT id, sender_id, getter_id, sended_time, text
             FROM chat_message
             WHERE sender_id = %s AND getter_id = %s OR
             sender_id = %s AND getter_id = %s
             '''
-    history = User.objects.raw(query, (cur_user_id, pk, pk, cur_user_id))
-    # read messages
+    history = Message.objects.raw(query, (str(cur_user_id),
+                                          pk, pk,
+                                          str(cur_user_id)))
+
+    Message.objects.filter(sender_id=pk,
+                           getter_id=cur_user_id,
+                           is_red=False).update(is_red=True)
+
     # send new
     return render(request, 'chat/chat_page.html',
                   {'history': history,
-                   'cur_id': int(cur_user_id)})
+                   'cur_id': cur_user_id})
