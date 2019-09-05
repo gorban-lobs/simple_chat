5d6b6e166a8db87387cf9e23de5cb1fcad46f5dc
Gorban Lobs <lobs.gorban@yandex.ru>
2019-09-05 14:04:35 +0300

    Add chats getting to chat_list

--- chat_project/chat/views.py
+++ chat_project/chat/views.py
@@ -7,7 +7,12 @@ from .models import Message
 
 @login_required
 def chat_list(request):
-    # cur_user = request.user
-    # print(cur_user)
-    chat_list = User.objects.filter()
+    cur_user_id = str(request.user.id)
+    query = '''
+            SELECT u.id, username, count(m.sender_id) as cnt
+            FROM auth_user u LEFT JOIN chat_message m ON
+            u.id = m.sender_id AND  m.is_red = False AND m.getter_id = %s
+            GROUP BY u.id HAVING u.id <> %s
+            '''
+    chat_list = User.objects.raw(query, (cur_user_id, cur_user_id))
     return render(request, 'chat/chat_list.html', {'chats': chat_list})
